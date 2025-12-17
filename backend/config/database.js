8342358cd8e92a03178fe6422be2a812a828f630
Yvon-Yves-OVONO-BENG <100134336+Yvon-Yves-OVONO-BENG@users.noreply.github.com>
2025-12-18 00:23:54 +0100

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("snapexam", "root", "", {
-  host: "localhost",
+const sequelize = new Sequelize("c2462367c_snapexam", "c2462367c_userSnapExam", "Uiz%4idZNfFv", {
+  host: "mysql.tophebergement.com",
   dialect: "mysql",
   logging: false, // optionnel : désactive les logs SQL dans la console
 });
